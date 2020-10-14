91d3f69e35f95be4d91d403975d899a2a37f369a
Geo Min <geom@vt.edu>
2020-10-13 22:04:47 -0400

    Added time lapse logic for number of photos

--- pilapse.py
+++ pilapse.py
@@ -27,19 +27,12 @@ Options:
 from docopt import docopt
 from picamera import PiCamera
 from time import sleep
-import os
+import os, shutil
 from getch import pause
 import logging
 
-# q = 0
-
 def pilapse(args):
-    if args['-v']:
-      logging.basicConfig(format='LOGGING: %(message)s', level=logging.DEBUG)
-    elif args['-q']:
-      logging.basicConfig(level=logging.CRITICAL)
-    else:
-      logging.basicConfig(format='LOGGING: %(message)s', level=logging.INFO)
+    loggingSetter(args, logging)
     
     logging.debug("Initializing PiCamera")
     camera = PiCamera()
@@ -51,18 +44,71 @@ def pilapse(args):
     logging.info("Previewing the camera frame")
     preview(camera)
 
+    logging.debug("Getting timelapse interval")
+    interval = getInterval(args)
+    logging.info("Setting timelapse interval of {0}".format(interval))
+
+    for i in range(5, 0, -1):
+      sleep(1)
+      logging.critical("Timelapse starting in {0}".format(str(i)))
+
     # print(args)
+    logging.debug("Creating tmp photo directory")
+    try:
+      os.mkdir('tmp')
+    except:
+      pass
+
+    if args['--photos']:
+      logging.info("Taking timelapse of {0} photos".format(args['VALUE']))
+      for i in range(int(args['VALUE'])):
+        logging.info("Taking photo {0}".format(str(i)))
+        camera.capture('tmp/{0}.jpg'.format(str(i)))
+        logging.debug("Saved in /tmp")
+        sleep(interval)
+        logging.debug("Taking a {0}s interval".format(str(interval)))
+    
+    
+    
+    logging.debug("Removing tmp photo directory")
+    shutil.rmtree('tmp/')
+
 
 
 def preview(camera):
-  logging.debug("Starting camera preview")
+    logging.debug("Starting camera preview")
+    camera.start_preview(fullscreen=False, window=(100, 100, 640, 480))
 
-  camera.start_preview(fullscreen=False, window=(100, 100, 640, 480))
-  pause('Position your frame for the timelapse. \nPress any key to exit preview.')
+    pause('Position your frame for the timelapse. \nPress any key to exit preview.')
 
-  logging.debug("Stopping camera preview")
-  camera.stop_preview()
+    logging.debug("Stopping camera preview")
+    camera.stop_preview()
 
+
+def loggingSetter(args, logging):
+    if args['-v']:
+      logging.basicConfig(format='LOGGING: %(message)s', level=logging.DEBUG)
+    elif args['-q']:
+      logging.basicConfig(level=logging.CRITICAL)
+    else:
+      logging.basicConfig(format='LOGGING: %(message)s', level=logging.INFO)
+
+def getInterval(args):
+    interval = 5
+    intervalStr = args['--interval']
+    if intervalStr.isdigit():
+      interval = int(intervalStr)      
+    elif intervalStr[-1] == 's':
+      interval = int(intervalStr[:-1])
+    elif intervalStr[-1] == 'm':
+      interval = int(intervalStr[:-1]) * 60
+    elif intervalStr[-1] == 'h':
+      interval = int(intervalStr[:-1]) * 3600
+    elif intervalStr[-1] == 'd':
+      interval = int(intervalStr[:-1]) * 86400
+    else:
+      raise Exception("Interval format not accepted")
+    return interval
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='PiLapse 1.0')
